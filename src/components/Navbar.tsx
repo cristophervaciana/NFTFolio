dcaf114d9150b51539fa16e746b10e3aba0bda17
Cristopher Vaciana <cris95vg@outlook.com>
2022-04-29 13:28:06 -0600

    Add btn to close session

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,16 @@ const Navbar: React.FC = () => {
   const [connectBtnText, setConnectBtnText] = useState("Connect Wallet");
   const [connectUser, setConnectUser] = useState(false);
 
+  const accountChangeHandler= () =>{
+    setConnectUser(false);
+    setConnectBtnText("Connect Wallet");
+  }
+
+  const closeSession = () =>{
+    accountChangeHandler();
+    (window as any).ethereum.removeListener('accountsChanged', accountChangeHandler);
+   // (window as any).location.reload();
+  }
   const connectWallet = async () => {
     try {
       if ((window as any).ethereum && (window as any).ethereum.isMetaMask) {
@@ -29,10 +39,10 @@ const Navbar: React.FC = () => {
     }
   };
 
-  (window as any).ethereum.on("accountsChanged", () => {
-    setConnectUser(false);
-    setConnectBtnText("Connect Wallet");
-  });
+  if(connectUser){
+    (window as any).ethereum.on("accountsChanged",accountChangeHandler);
+  }
+
 
   return (
     <>
@@ -42,7 +52,7 @@ const Navbar: React.FC = () => {
             <div className="flex flex-shrik md:w-1/3 justify-center md:justify-start text white mt-3">
               <a href="/" aria-label="Home">
                 <span className="text-xl pl-2 text-white">
-                  <EthLogo className=" pl-4 inline w-10 h-10" />
+                  <EthLogo className=" pl-4 inline w-10 h-10 fill-white" />
 
                   <h1 className="inline pl-4 pt-4">NFTFolio</h1>
                 </span>
@@ -82,6 +92,22 @@ const Navbar: React.FC = () => {
                     </button>
                   </div>
                 </li>
+
+                {connectUser ? (
+                  <li className="flex-1 md:flex-none md:mr-3">
+                    <div className="relative inline-block">
+                      <button  className="drop-button text-white py-1 px-2 border-2 border-gray-500 rounded-xl" onClick={closeSession}>
+                        <svg
+                          className="h-8 w-8 inline py-2 fill-white"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 512 512"
+                        >
+                          <path d="M160 416H96c-17.67 0-32-14.33-32-32V128c0-17.67 14.33-32 32-32h64c17.67 0 32-14.33 32-32S177.7 32 160 32H96C42.98 32 0 74.98 0 128v256c0 53.02 42.98 96 96 96h64c17.67 0 32-14.33 32-32S177.7 416 160 416zM502.6 233.4l-128-128c-12.51-12.51-32.76-12.49-45.25 0c-12.5 12.5-12.5 32.75 0 45.25L402.8 224H192C174.3 224 160 238.3 160 256s14.31 32 32 32h210.8l-73.38 73.38c-12.5 12.5-12.5 32.75 0 45.25s32.75 12.5 45.25 0l128-128C515.1 266.1 515.1 245.9 502.6 233.4z" />
+                        </svg>
+                      </button>
+                    </div>
+                  </li>
+                ) : null}
               </ul>
             </div>
           </div>
